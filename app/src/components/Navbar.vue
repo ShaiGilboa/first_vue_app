7b3642265cc3845a4ef4c8035d47217601012c44
Shai Gilboa <shizel@gmail.com>
2020-08-13 12:03:27 -0400

    initial 2

---
+++ app/src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id='Navbar' data-css='Navbar' class='' >
+    
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Navbar',
+    props: {
+      
+    }
+  }
+</script>
+
+<style scoped>
+  
+</style>
---
+++ app/src/components/Product.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id='Product' data-css='Product' class='' >
+    <div class="product-image">
+      <img v-bind:src='imageSrc' />
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Product',
+    props: {
+      imageSrc: String,
+    }
+  }
+</script>
+
+<style scoped>
+  
+</style>
